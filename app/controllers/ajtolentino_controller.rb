6224030d2b9527fb9e9be411eae8c1467c5cd796
ajibtolentino <ajibtolentino@outlook.com>
2024-04-20 14:19:08 +0800

    proj

--- app/controllers/ajtolentino_controller.rb
+++ app/controllers/ajtolentino_controller.rb
@@ -1,8 +1,7 @@
 class AjtolentinoController < ApplicationController
   def index
   end
-
-  def download
+  def download_cv
     file_path = Rails.root.join('app', 'assets', 'ajresume.pdf')
     send_file file_path, filename: 'ajresume.pdf', type: 'application/pdf', disposition: 'attachment'
   end
